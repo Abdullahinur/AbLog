6b91e81865a983ccb63eea3db84c3fa103307aa6
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-25 08:38:50 +0300

    make it better

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    pass
